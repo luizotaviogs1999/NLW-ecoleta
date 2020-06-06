7a2b79f2b4083361a606bd37b9ae42f4e32fefc8
Luiz Otavio <luizotaviogs1999@gmail.com>
2020-06-05 23:12:19 -0300

    Aula #3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,39 +1,73 @@
 function populationUFs() {
-  const ufSelect = document.querySelector("select[name=uf]")
+  const ufSelect = document.querySelector("select[name=uf]");
 
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res => res.json())
-    .then(states => {
+    .then((res) => res.json())
+    .then((states) => {
       for (const state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
       }
-    })
+    });
 }
 
-populationUFs()
+populationUFs();
 
 function getCities(event) {
-  const citySelect = document.querySelector("select[name=city]")
-  const stateInput = document.querySelector("[name=state]")
+  const citySelect = document.querySelector("select[name=city]");
+  const stateInput = document.querySelector("[name=state]");
 
-  const ufValue = event.target.value
+  const ufValue = event.target.value;
 
-  const indexOfSelectedState = event.target.selectedIndex
-  stateInput.value = event.target.options[indexOfSelectedState].text
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+  citySelect.innerHTML = "<option value> Seleciona Estado </option>";
+  citySelect.disabled = true;
   fetch(url)
-    .then(res => res.json())
-    .then(cities => {
+    .then((res) => res.json())
+    .then((cities) => {
       for (const city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
-    })
-  citySelect.disabled = false
-
+    });
+  citySelect.disabled = false;
 }
 
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
 
-document.querySelector("select[name=uf]")
-  .addEventListener("change", getCities)
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+for (let item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // adicionar ou remover uma classe com javascript
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.findIndex((item) => {
+    const itemFound = item == itemId; // isso será true ou false
+    return itemFound;
+  });
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId; // false
+      return itemIsDifferent;
+    });
+
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
